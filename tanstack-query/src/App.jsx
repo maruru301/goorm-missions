5e813a28720d17d8f2553cc6abda9cbf245f9be9
윤진 <151013932+maruru301@users.noreply.github.com>
2025-11-18 15:17:05 +0900

    feat: todo 조회 및 로딩/에러 처리 추가

--- tanstack-query/src/App.jsx
+++ tanstack-query/src/App.jsx
@@ -1,7 +1,37 @@
 import './App.css';
 
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const BASE_URL = 'http://localhost:4000/todos';
+
 function App() {
-    return <div>app</div>;
+    const fetchTodos = async () => {
+        const res = await axios.get(BASE_URL);
+        return res.data;
+    };
+
+    const {
+        data: todos,
+        isPending,
+        isError,
+    } = useQuery({
+        queryKey: ['todos'],
+        queryFn: fetchTodos,
+    });
+
+    if (isPending) return <div>로딩중...</div>;
+    if (isError) return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
+
+    return (
+        <div>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
