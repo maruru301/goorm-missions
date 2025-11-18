9ad4f456c870d1844da83a67ae17ae82dabd692d
윤진 <151013932+maruru301@users.noreply.github.com>
2025-11-18 16:29:44 +0900

    refactor: Todos 컴포넌트 분리 및 CSS 구조 정리

---
+++ tanstack-query/src/mission/Todos.jsx
@@ -0,0 +1,102 @@
+import './Todos.css';
+
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import axios from 'axios';
+import { useState } from 'react';
+
+const BASE_URL = 'http://localhost:4000/todos';
+
+const Todos = () => {
+    const [todo, setTodo] = useState('');
+    const queryClient = useQueryClient();
+
+    const fetchTodos = async () => {
+        const res = await axios.get(BASE_URL);
+        return res.data;
+    };
+
+    const {
+        data: todos,
+        isPending,
+        isError,
+    } = useQuery({
+        queryKey: ['todos'],
+        queryFn: fetchTodos,
+    });
+
+    const addTodo = async (newTodo) => {
+        const res = await axios.post(BASE_URL, newTodo);
+        return res.data;
+    };
+
+    const { mutate: addMutate } = useMutation({
+        mutationFn: addTodo,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['todos']); // refetching
+            setTodo('');
+        },
+    });
+
+    const toggleTodo = async (todo) => {
+        const res = await axios.patch(`${BASE_URL}/${todo.id}`, {
+            completed: !todo.completed,
+        });
+
+        return res.data;
+    };
+
+    const { mutate: toggleMutate } = useMutation({
+        mutationFn: toggleTodo,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['todos']);
+        },
+    });
+
+    const deleteTodo = async (todo) => {
+        await axios.delete(`${BASE_URL}/${todo.id}`);
+    };
+
+    const { mutate: deleteMutate } = useMutation({
+        mutationFn: deleteTodo,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['todos']);
+        },
+    });
+
+    if (isPending) return <div>로딩중...</div>;
+    if (isError) return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
+
+    return (
+        <div>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    addMutate({ title: todo, completed: false });
+                }}
+            >
+                <input value={todo} onChange={(e) => setTodo(e.target.value)} type="text" />
+                <button>추가</button>
+            </form>
+
+            <ul className="todo-list-container">
+                {todos.map((todo) => (
+                    <div key={todo.id} className="todo-list">
+                        <li onClick={() => toggleMutate(todo)} className="todo-item">
+                            <span className={`todo-title ${todo.completed ? 'completed' : 'in-progress'}`}>
+                                {todo.title}
+                            </span>
+                            <span className={`completed-badge ${todo.completed ? 'completed' : 'in-progress'}`}>
+                                {todo.completed ? '완료' : '진행중'}
+                            </span>
+                        </li>
+
+                        <button onClick={() => deleteMutate(todo)}>삭제</button>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Todos;
