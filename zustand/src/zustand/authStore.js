43d47afd38d7e3ce25350f76db88b2a1e33c025b
윤진 <151013932+maruru301@users.noreply.github.com>
2025-11-21 15:59:55 +0900

    feat: persist를 적용하여 로그인 상태 로컬 저장

--- zustand/src/zustand/authStore.js
+++ zustand/src/zustand/authStore.js
@@ -1,35 +1,43 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import supabase from '../supabase/supabaseClient';
 
-const useAuthStore = create((set) => ({
-    userEmail: null, // 로그인한 사용자 이메일
-    isLogin: false, // 로그인 상태
+const useAuthStore = create(
+    persist(
+        (set) => ({
+            userEmail: null, // 로그인한 사용자 이메일
+            isLogin: false, // 로그인 상태
 
-    login: async ({ email, password }) => {
-        try {
-            const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+            login: async ({ email, password }) => {
+                try {
+                    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
 
-            if (error) throw error;
+                    if (error) throw error;
 
-            set({ userEmail: data.user?.email, isLogin: true });
-        } catch (error) {
-            alert(error.message);
-            console.error('로그인 오류:', error);
+                    set({ userEmail: data.user?.email, isLogin: true });
+                } catch (error) {
+                    alert(error.message);
+                    console.error('로그인 오류:', error);
+                }
+            },
+
+            logout: async () => {
+                try {
+                    const { error } = await supabase.auth.signOut();
+
+                    if (error) throw error;
+
+                    set({ userEmail: null, isLogin: false });
+                } catch (error) {
+                    alert(error.message);
+                    console.error('로그아웃 오류: ', error);
+                }
+            },
+        }),
+        {
+            name: 'auth-storage',
         }
-    },
-
-    logout: async () => {
-        try {
-            const { error } = await supabase.auth.signOut();
-
-            if (error) throw error;
-
-            set({ userEmail: null, isLogin: false });
-        } catch (error) {
-            alert(error.message);
-            console.error('로그아웃 오류: ', error);
-        }
-    },
-}));
+    )
+);
 
 export default useAuthStore;
