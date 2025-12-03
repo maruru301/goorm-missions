0b0da8b7d3944665ea1bd1097c7e842b6efc034b
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-03 17:10:25 +0900

    style: 홈페이지 스타일 적용

--- next-rendering/src/app/page.tsx
+++ next-rendering/src/app/page.tsx
@@ -4,12 +4,13 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-        <div>
-            <h1>환영합니다</h1>
+        <div className="min-h-screen flex flex-col items-center justify-center p-6">
+            <h1 className="text-3xl font-bold">환영합니다!</h1>
+            <p className="text-gray-300 mb-6 text-center pt-2">아래 버튼을 눌러 다양한 책들을 확인해보세요.</p>
 
             <nav>
                 <Link href="/books">
-                    <button>책 목록 보기</button>
+                    <button className="bg-blue-500 px-4 py-1 rounded hover:bg-blue-600 text-sm">책 목록 보기</button>
                 </Link>
             </nav>
         </div>
