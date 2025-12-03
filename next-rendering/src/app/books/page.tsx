25415cec3aa39b21caf500fd463995735d6f5cd5
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-03 16:23:19 +0900

    feat: 책 목록 페이지 구현 (ISR)

--- next-rendering/src/app/books/page.tsx
+++ next-rendering/src/app/books/page.tsx
@@ -1,9 +1,34 @@
 // 책 목록 페이지 (ISR)
 
-import React from 'react';
+import { Book } from '@/types/book';
 
-const page = () => {
-    return <div>책 목록</div>;
+const page = async () => {
+    const res = await fetch('http://localhost:4000/books', {
+        next: { revalidate: 10 }, // 10초마다 백그라운드에서 재생성
+    });
+    const books: Book[] = await res.json();
+
+    return (
+        <div>
+            <h2>책 목록</h2>
+
+            <div>
+                {books.map((book) => (
+                    <div key={book.id}>
+                        <div>
+                            <img src={book.coverImage} alt={book.title} width={200} />
+
+                            <div>
+                                <h2>{book.title}</h2>
+                                <p>{book.author}</p>
+                                <p>{book.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default page;
