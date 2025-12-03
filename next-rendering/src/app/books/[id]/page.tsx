2f4ae85fd6563e9b5adde05bd98e93afa809d126
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-03 16:54:18 +0900

    feat: /books/[id] 페이지 SSR 렌더링 및 스타일 적용

--- next-rendering/src/app/books/[id]/page.tsx
+++ next-rendering/src/app/books/[id]/page.tsx
@@ -1,5 +1,7 @@
 // 책 상세 페이지 (SSR)
 
+import { Book } from '@/types/book';
+
 type Params = {
     params: Promise<{ id: number }>;
 };
@@ -7,7 +9,60 @@ type Params = {
 const page = async ({ params }: Params) => {
     const { id } = await params;
 
-    return <div>책 상세 정보 ({id})</div>;
+    const res = await fetch(`http://localhost:4000/books/${id}`, {
+        cache: 'no-store', // 매 요청마다 새로 가져오기
+    });
+
+    if (!res.ok) {
+        // 데이터 없을 때
+        return <div className="p-10 font-bold">책을 찾을 수 없습니다.</div>;
+    }
+
+    const book: Book = await res.json();
+
+    return (
+        <div className="p-10 flex gap-6">
+            <img src={book.coverImage} alt={book.title} width={300} />
+
+            <div className="flex flex-col gap-2">
+                <div>
+                    <div className="flex items-center gap-2 mb-2">
+                        <h1 className="text-2xl font-bold">{book.title}</h1>
+
+                        <div>
+                            {book.isNew && (
+                                <span className="bg-green-200 text-green-800 text-xs font-bold px-2 py-1 rounded-full">
+                                    신간
+                                </span>
+                            )}
+                        </div>
+                    </div>
+
+                    <p className="text-gray-400">{book.author}</p>
+                    <p className="mb-4">{book.description}</p>
+                </div>
+
+                <div className="mt-4 flex flex-col gap-2">
+                    <p className="mt-2 font-semibold">
+                        {book.price.amount} {book.price.currencyCode}
+                    </p>
+
+                    <p className="text-yellow-500">★ {book.rating}</p>
+
+                    <div className="flex flex-wrap gap-2 mt-2">
+                        {book.tags.map((tag) => (
+                            <span
+                                key={tag}
+                                className="bg-gray-300 text-gray-800 text-xs font-semibold px-2 py-1 rounded-full"
+                            >
+                                {tag}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default page;
