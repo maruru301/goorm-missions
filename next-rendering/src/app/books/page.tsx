d51eb209db9632482fdec429dfe7a3594d2fe1ab
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-03 17:03:31 +0900

    style: /books 페이지 스타일 적용

--- next-rendering/src/app/books/page.tsx
+++ next-rendering/src/app/books/page.tsx
@@ -10,24 +10,38 @@ const page = async () => {
     const books: Book[] = await res.json();
 
     return (
-        <div>
-            <h2>책 목록</h2>
+        <div className="p-8">
+            <h2 className="text-3xl font-bold mb-6">책 목록</h2>
 
-            <div>
+            <div className="flex flex-col gap-6">
                 {books.map((book) => (
-                    <div key={book.id}>
-                        <div>
-                            <img src={book.coverImage} alt={book.title} width={200} />
+                    <div key={book.id} className="border flex gap-4">
+                        <img src={book.coverImage} alt={book.title} width={200} />
 
-                            <div>
-                                <h2>{book.title}</h2>
-                                <p>{book.author}</p>
-                                <p>{book.description}</p>
+                        <div className="pt-4">
+                            <div className="flex items-center gap-2">
+                                <h1 className="text-2xl font-bold">{book.title}</h1>
+
+                                <div>
+                                    {book.isNew && (
+                                        <span className="bg-green-200 text-green-800 text-xs font-bold px-2 py-1 rounded-full">
+                                            신간
+                                        </span>
+                                    )}
+                                </div>
                             </div>
 
-                            <Link href={`/books/${book.id}`}>
-                                <button>자세히 보기</button>
-                            </Link>
+                            <p className="text-gray-400">{book.author}</p>
+                            <p className="mb-4">{book.description}</p>
+                            <p className="text-yellow-500">★ {book.rating}</p>
+
+                            <div>
+                                <Link href={`/books/${book.id}`}>
+                                    <button className="bg-blue-500 px-4 py-1 rounded hover:bg-blue-600 text-sm">
+                                        자세히 보기
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 ))}
